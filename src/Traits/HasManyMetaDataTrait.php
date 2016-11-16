6649314709d9c82ec22a4acd67627b1d45b154dd
Astrit Zeqiri <astritzeqiri5@gmail.com>
2016-11-16 14:41:59 +0100

    PSR-2 on the meta data trait.

--- src/Traits/HasManyMetaDataTrait.php
+++ src/Traits/HasManyMetaDataTrait.php
@@ -1,7 +1,9 @@
 <?php 
 
 namespace AstritZeqiri\Metadata\Traits;
+
 use AstritZeqiri\Metadata\Models\Metadata as Metadata;
+
 /**
  * This is the has many media trait.
  *
@@ -12,10 +14,11 @@ trait HasManyMetaDataTrait
      * This method runs when the trait has booted
      * 
      */
-    public static function bootHasManyMetaDataTrait(){
+    public static function bootHasManyMetaDataTrait()
+    {
         $name = static::class;
         // Add a listener on deleting a object and delete all of its metas
-        \Event::listen("eloquent.deleting: {$name}", function($model){
+        \Event::listen("eloquent.deleting: {$name}", function ($model) {
             $model->delete_all_metas();
         });
     }
@@ -37,18 +40,20 @@ trait HasManyMetaDataTrait
      */
     public function update_meta($key = null, $value = null)
     {
-        if($key == null || $value == null) return false;
+        if ($key == null || $value == null) {
+            return false;
+        }
 
         $exists_meta = $this->get_meta($key);
         
-        if($exists_meta == null){
+        if ($exists_meta == null) {
             // create a new meta_data
             $meta = $this->meta_data()->create([
                 'key' => $key,
                 'value' => $value
             ]);
             return $meta;
-        }else{
+        } else {
             // update the existing meta_data
             $exists_meta->update(['value' => $value]);
             return $exists_meta;
@@ -65,8 +70,9 @@ trait HasManyMetaDataTrait
      */
     public function get_meta($key = null, $get_value = false)
     {
-        if(!$key || ($meta = $this->meta_data()->whereKey($key)->first()) == null)
+        if (!$key || ($meta = $this->meta_data()->whereKey($key)->first()) == null) {
             return $get_value == true ? "" : null;
+        }
 
         return $get_value == true ? $meta->value : $meta;
     }
@@ -80,11 +86,13 @@ trait HasManyMetaDataTrait
      */
     public function delete_meta($key = null)
     {
-        if(!$key) return false;
+        if (!$key) {
+            return false;
+        }
 
         $meta = $this->get_meta($key);
         
-        if($meta){
+        if ($meta) {
             return $meta->delete();
         }
         return false;
@@ -97,7 +105,7 @@ trait HasManyMetaDataTrait
      */
     public function delete_all_metas()
     {
-        $this->meta_data->each(function($model){ $model->delete(); });
+        $this->meta_data->each(function ($model) { $model->delete(); });
     }
 
     /**
@@ -110,8 +118,9 @@ trait HasManyMetaDataTrait
      * 
      * @return $query
      */
-    public function createQueryForMeta($key = null, $value = null, $compare = "="){
-        return function($query) use($key, $value, $compare){
+    public function createQueryForMeta($key = null, $value = null, $compare = "=")
+    {
+        return function ($query) use ($key, $value,$compare) {
             $query->where('key', $key)->where('value', $compare, $value);
         };
     }
@@ -125,26 +134,33 @@ trait HasManyMetaDataTrait
      * 
      * @return $query
      */
-    public function scopeMetaQuery($query, $values = array(), $relation = "AND"){
-        if(!is_array($values)) return $query;
+    public function scopeMetaQuery($query, $values = array(), $relation = "AND")
+    {
+        if (!is_array($values)) {
+            return $query;
+        }
 
         $relations = array("OR", "AND");
         
-        if(!in_array($relation, $relations)) $relation = "AND";
-
-        $values = $this->filterMetaQueryArray($values);
-        
-        if(empty($values)) return $query;
-        
-        if(count($values) == 1){
+        if (!in_array($relation, $relations)) {
             $relation = "AND";
         }
 
-        return $query->where(function($query) use($values, $relation){
-            foreach($values as $value){
-                if($relation == "OR"){
+        $values = $this->filterMetaQueryArray($values);
+        
+        if (empty($values)) {
+            return $query;
+        }
+        
+        if (count($values) == 1) {
+            $relation = "AND";
+        }
+
+        return $query->where(function ($query) use ($values,$relation) {
+            foreach ($values as $value) {
+                if ($relation == "OR") {
                     $query->orWhereHas('meta_data', $this->createQueryForMeta($value['key'], $value['value'], $value['compare']));
-                }else if($relation == "AND"){
+                } elseif ($relation == "AND") {
                     $query->whereHas('meta_data', $this->createQueryForMeta($value['key'], $value['value'], $value['compare']));
                 }
             }
@@ -156,8 +172,9 @@ trait HasManyMetaDataTrait
      * @param  array  $array [description]
      * @return [type]        [description]
      */
-    public function filterMetaQueryArray($array = array()){
-        return array_values(array_filter(array_map(function($item){
+    public function filterMetaQueryArray($array = array())
+    {
+        return array_values(array_filter(array_map(function ($item) {
             return $this->makeMetaQueryArrayItem($item);
         }, $array)));
     }
@@ -169,12 +186,19 @@ trait HasManyMetaDataTrait
      * 
      * @return array or false (return the array of return false) 
      */
-    public function makeMetaQueryArrayItem($item = array()){
-        if(!$item || !is_array($item)) return false;
+    public function makeMetaQueryArrayItem($item = array())
+    {
+        if (!$item || !is_array($item)) {
+            return false;
+        }
         
-        if(!isset($item['key']) || !isset($item['value'])) return false;
+        if (!isset($item['key']) || !isset($item['value'])) {
+            return false;
+        }
 
-        if(!isset($item['compare'])) $item['compare'] = "=";
+        if (!isset($item['compare'])) {
+            $item['compare'] = "=";
+        }
 
         return $item;
     }
