41f1959cfd31ac8719fc036ddd944d17dc997b71
Astrit Zeqiri <astritzeqiri5@gmail.com>
2017-02-27 15:13:43 +0100

    Indentation fix on Metadata Model

--- src/Models/MetaData.php
+++ src/Models/MetaData.php
@@ -7,33 +7,33 @@ use AstritZeqiri\Metadata\Traits\HasRelatedObjectTrait;
 
 class MetaData extends Model
 {
-  use HasRelatedObjectTrait;
+    use HasRelatedObjectTrait;
 
-  protected $guarded = array();
-  
-  /**
-   * The attribute that shows if the table has timestamps
-   * 
-   * @var boolean
-   */
-  public $timestamps = false;
+    protected $guarded = array();
+    
+    /**
+     * The attribute that shows if the table has timestamps
+     * 
+     * @var boolean
+     */
+    public $timestamps = false;
 
-  /**
-   * The table name 
-   * 
-   * @var string
-   */
-  protected $table = 'meta_datas';
+    /**
+     * The table name 
+     * 
+     * @var string
+     */
+    protected $table = 'meta_datas';
 
-  /**
-   * The attributes that are mass assignable.
-   *
-   * @var array
-   */
-  protected $fillable = array(
-    'key',
-    'value',
-    'object_id',
-    'object_type',
-  );
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = array(
+        'key',
+        'value',
+        'object_id',
+        'object_type',
+    );
 }
