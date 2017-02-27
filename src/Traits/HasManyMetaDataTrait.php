4d90c1d89fea8697877438a500d4b6724860e072
Astrit Zeqiri <astritzeqiri5@gmail.com>
2017-02-27 15:14:32 +0100

    Some refactoring on the hasManyMetaData Trait

--- src/Traits/HasManyMetaDataTrait.php
+++ src/Traits/HasManyMetaDataTrait.php
@@ -70,8 +70,14 @@ trait HasManyMetaDataTrait
      */
     public function get_meta($key = null, $get_value = false)
     {
-        if (!$key || ($meta = $this->meta_data()->whereKey($key)->first()) == null) {
-            return $get_value == true ? "" : null;
+        if (! $key) {
+            return null;
+        }
+
+        $meta = $this->meta_data()->whereKey($key)->first();
+
+        if (! $meta) {
+            return null;
         }
 
         return $get_value == true ? $meta->value : $meta;
@@ -86,16 +92,17 @@ trait HasManyMetaDataTrait
      */
     public function delete_meta($key = null)
     {
-        if (!$key) {
+        if (! $key) {
             return false;
         }
 
         $meta = $this->get_meta($key);
         
-        if ($meta) {
-            return $meta->delete();
+        if (! $meta) {
+            return false;
         }
-        return false;
+
+        return $meta->delete();
     }
     
     /**
@@ -109,23 +116,6 @@ trait HasManyMetaDataTrait
     }
 
     /**
-     *  return only one meta query filter
-     * 
-     * @param $query
-     * @param string $key the meta key
-     * @param string $value the meta value
-     * @param string $compare the meta compare
-     * 
-     * @return $query
-     */
-    public function createQueryForMeta($key = null, $value = null, $compare = "=")
-    {
-        return function ($query) use ($key, $value,$compare) {
-            $query->where('key', $key)->where('value', $compare, $value);
-        };
-    }
-
-    /**
      *  Filter items by a given array of meta_datas
      * 
      * @param $query
@@ -136,14 +126,8 @@ trait HasManyMetaDataTrait
      */
     public function scopeMetaQuery($query, $values = array(), $relation = "AND")
     {
-        if (!is_array($values)) {
+        if (! is_array($values)) {
             return $query;
-        }
-
-        $relations = array("OR", "AND");
-        
-        if (!in_array($relation, $relations)) {
-            $relation = "AND";
         }
 
         $values = $this->filterMetaQueryArray($values);
@@ -151,55 +135,119 @@ trait HasManyMetaDataTrait
         if (empty($values)) {
             return $query;
         }
+
+        $relation = $this->resolveRelation($relation);
         
         if (count($values) == 1) {
             $relation = "AND";
         }
 
-        return $query->where(function ($query) use ($values,$relation) {
+        return $query->where(function ($query) use ($values, $relation) {
+            $method = $this->getMethodFromRelation($relation);
+
             foreach ($values as $value) {
-                if ($relation == "OR") {
-                    $query->orWhereHas('meta_data', $this->createQueryForMeta($value['key'], $value['value'], $value['compare']));
-                } elseif ($relation == "AND") {
-                    $query->whereHas('meta_data', $this->createQueryForMeta($value['key'], $value['value'], $value['compare']));
-                }
+                $query->$method(
+                    'meta_data',
+                    $this->createQueryForMeta(
+                        $value['key'],
+                        $value['value'],
+                        $value['compare']
+                    )
+                );
             }
         });
     }
 
     /**
-     * Filter the meta query array values
-     * @param  array  $array [description]
-     * @return [type]        [description]
+     * Return only one meta query filter.
+     * 
+     * @param $query
+     * @param string $key the meta key
+     * @param string $value the meta value
+     * @param string $compare the meta compare
+     * 
+     * @return $query
      */
-    public function filterMetaQueryArray($array = array())
+    private function createQueryForMeta($key, $value, $compare = "=")
     {
-        return array_values(array_filter(array_map(function ($item) {
+        return function ($query) use ($key, $value, $compare) {
+            $query->where('key', $key)->where('value', $compare, $value);
+        };
+    }
+
+    /**
+     * Filter the meta query array values.
+     * 
+     * @param  array $items
+     * 
+     * @return array
+     */
+    private function filterMetaQueryArray($items = [])
+    {
+        return collect($items)
+        ->map(function ($item) {
             return $this->makeMetaQueryArrayItem($item);
-        }, $array)));
+        })
+        ->filter()
+        ->values()
+        ->toArray();
     }
 
     /**
      * Make a metaquery array item (Item example ["item" => "", "value" => "", "compare" => ""])]
      * 
-     * @param  array  $item [description]
+     * @param  array  $item
      * 
-     * @return array or false (return the array of return false) 
+     * @return array|null 
      */
-    public function makeMetaQueryArrayItem($item = array())
+    private function makeMetaQueryArrayItem($item = [])
     {
-        if (!$item || !is_array($item)) {
-            return false;
+        if (! is_array($item)) {
+            return null;
         }
         
-        if (!isset($item['key']) || !isset($item['value'])) {
-            return false;
+        if (! array_key_exists("key", $item) || ! array_key_exists("value", $item)) {
+            return null;
         }
 
-        if (!isset($item['compare'])) {
+        if (! array_key_exists("compare", $item)) {
             $item['compare'] = "=";
         }
 
         return $item;
+    }
+
+    /**
+     * Get the method from the relation.
+     * 
+     * @param  string $relation
+     * 
+     * @return string
+     */
+    private function getMethodFromRelation($relation = 'AND')
+    {
+        if ($relation == "OR") {
+            return 'orWhereHas';
+        }
+
+        $method = 'whereHas';
+    }
+
+    /**
+     * Resolve the relation.
+     * 
+     * @param  string $relation
+     * 
+     * @return string $relation
+     */
+    private function resolveRelation($relation)
+    {
+        $relations = array("OR", "AND");
+        
+        if (! in_array($relation, $relations)) {
+            $relation = "AND";
+        }
+
+        return $relation;
     }
 }
